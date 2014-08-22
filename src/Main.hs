0a75b808b1b4d4dda89f5b8079910fc4e56c615d
Chris Allen <cma@bitemyapp.com>
2014-08-22 16:52:53 -0500

    working

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
+import Control.Monad (replicateM)
 import Control.Monad.IO.Class
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Char8 as BC
 import Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import qualified Data.Text.Lazy as TL
 import Data.Word8
@@ -12,8 +14,10 @@ import Network.URI (parseURI)
 import qualified System.Random as SR
 import Web.Scotty
 
-shortyGen :: SR.RandomGen b => b -> BS.ByteString
-shortyGen = encodeUtf8 . TL.toStrict . TL.pack . fst . randomString 7
+alphaNum = ['A'..'Z'] ++ ['0'..'9']
+randomElement l = SR.randomRIO (0, length l - 1) >>= return . (l !!)
+
+shortyGen = replicateM 7 (randomElement alphaNum)
 
 saveURI conn shortURI uri = R.runRedis conn $ R.set shortURI uri
 getURI conn shortURI = R.runRedis conn $ R.get shortURI
@@ -24,10 +28,10 @@ main = scotty 3000 $ do
     uri <- param "uri"
     case parseURI (TL.unpack uri) of
       Just _  -> do
-        g <- liftIO SR.getStdGen
-        let shorty = shortyGen g
+        shawty <- liftIO shortyGen
+        let shorty = BC.pack shawty
         resp <- liftIO (saveURI rConn shorty (encodeUtf8 (TL.toStrict uri)))
-        text $ TL.concat [(TL.pack (show resp)), " shorty is: ", TL.fromStrict (decodeUtf8 shorty)]
+        text $ TL.concat [(TL.pack (show resp)), " shorty is: ", TL.pack shawty]
       Nothing -> text (TL.concat [uri, " wasn't a url"])
   get "/:short" $ do
     short <- param "short"
