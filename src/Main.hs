b06ea420d7b3d0b2868ff4c5e81756da35b6f6ab
Chris Allen <cma@bitemyapp.com>
2014-08-22 16:56:54 -0500

    43 lines

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,13 @@
 module Main where
 
 import Control.Monad (replicateM)
-import Control.Monad.IO.Class
+import Control.Monad.IO.Class (liftIO)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Char8 as BC
 import Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import qualified Data.Text.Lazy as TL
-import Data.Word8
 import qualified Database.Redis as R
-import Network.Mail.Mime (randomString)
 import Network.URI (parseURI)
 import qualified System.Random as SR
 import Web.Scotty
